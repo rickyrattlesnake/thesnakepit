76c9e76becc020bb6b8d689ef4f86ccc5a3f6730
rattlesnake <ricky.ratnayake@gmail.com>
2017-06-21 23:57:04 +1000

    created webpack project with react

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default class App extends React.Component {
+    render() {
+        return (
+        <div style={{textAlign: 'centre'}} >
+            <h1>Hello From React</h1>
+        </div>);
+    }
+}
